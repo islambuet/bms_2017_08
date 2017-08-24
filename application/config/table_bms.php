3d3242b68df8f78de570ddffaf9c62588fe33850
Bipul Roy <bbipulroy@users.noreply.github.com>
2017-08-24 14:57:27 +0600

    Add minimum stock setup table in table_bms.php file

--- application/config/table_bms.php
+++ application/config/table_bms.php
@@ -4,6 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // Setup
 //Market Size
 $config['table_bms_setup_market_size'] = 'arm_bms_2017_08.bms_setup_market_size';
+//Minimum Stock
+$config['table_bms_setup_bud_stock_minimum'] = 'arm_bms_2017_08.bms_setup_bud_stock_minimum';
+
+
 //TI budget
 $config['table_bms_ti_budget_outlet'] = 'arm_bms_2017_08.bms_ti_budget_outlet';//outlet budget and target table
 $config['table_bms_ti_budget_ti'] = 'arm_bms_2017_08.bms_ti_budget_ti';//ti budget and target table
