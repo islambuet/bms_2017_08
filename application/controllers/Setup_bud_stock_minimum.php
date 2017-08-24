fa76383c2502134f79f19130d2a6f34b90ddeb95
Bipul Roy <bbipulroy@users.noreply.github.com>
2017-08-24 15:36:30 +0600

    Added controller Setup_bud_stock_minimum

---
+++ application/controllers/Setup_bud_stock_minimum.php
@@ -0,0 +1,197 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Setup_bud_stock_minimum extends Root_Controller
+{
+    private  $message;
+    public $permissions;
+    public $controller_url;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->message="";
+        $this->permissions=User_helper::get_permission('Setup_bud_stock_minimum');
+        $this->controller_url='setup_bud_stock_minimum';
+    }
+
+    public function index($action="list",$id=0)
+    {
+        if($action=="list")
+        {
+            $this->system_list();
+        }
+        elseif($action=="get_items")
+        {
+            $this->system_get_items();
+        }
+        elseif($action=="edit")
+        {
+            $this->system_edit($id);
+        }
+        elseif($action=="save")
+        {
+            $this->system_save();
+        }
+        else
+        {
+            $this->system_list();
+        }
+    }
+
+    private function system_list()
+    {
+        if(isset($this->permissions['action0']) && ($this->permissions['action0']==1))
+        {
+            $data['title']="Varieties Minimum Stock List";
+            $ajax['status']=true;
+            $ajax['system_content'][]=array("id"=>"#system_content","html"=>$this->load->view($this->controller_url."/list",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $ajax['system_page_url']=site_url($this->controller_url);
+            $this->json_return($ajax);
+        }
+        else
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->json_return($ajax);
+        }
+    }
+    private function system_get_items()
+    {
+        $this->db->from($this->config->item('table_login_setup_classification_varieties').' v');
+        $this->db->select('vms.quantity');
+        $this->db->select('v.id,v.name variety_name');
+        $this->db->select('crop.name crop_name');
+        $this->db->select('type.name crop_type_name');
+        $this->db->join($this->config->item('table_login_setup_classification_crop_types').' type','type.id = v.crop_type_id','INNER');
+        $this->db->join($this->config->item('table_login_setup_classification_crops').' crop','crop.id = type.crop_id','INNER');
+        $this->db->join($this->config->item('table_bms_setup_bud_stock_minimum').' vms','vms.variety_id = v.id','LEFT');
+        $this->db->order_by('crop.ordering','ASC');
+        $this->db->order_by('type.ordering','ASC');
+        $this->db->order_by('v.ordering','ASC');
+        $this->db->where('v.status !=',$this->config->item('system_status_delete'));
+        $this->db->where('v.whose','ARM');
+        $items=$this->db->get()->result_array();
+        $this->json_return($items);
+    }
+    private function system_edit($id)
+    {
+        if(isset($this->permissions['action2']) && ($this->permissions['action2']==1))
+        {
+            if($id>0)
+            {
+                $item_id=$id;
+            }
+            else
+            {
+                $item_id=$this->input->post('id');
+            }
+            $this->db->from($this->config->item('table_login_setup_classification_varieties').' v');
+            $this->db->select('vms.quantity');
+            $this->db->select('v.id,v.name variety_name');
+            $this->db->select('crop.name crop_name');
+            $this->db->select('type.name crop_type_name');
+            $this->db->join($this->config->item('table_login_setup_classification_crop_types').' type','type.id = v.crop_type_id','INNER');
+            $this->db->join($this->config->item('table_login_setup_classification_crops').' crop','crop.id = type.crop_id','INNER');
+            $this->db->join($this->config->item('table_bms_setup_bud_stock_minimum').' vms','vms.variety_id = v.id','LEFT');
+            $this->db->where('v.status !=',$this->config->item('system_status_delete'));
+            $this->db->where('v.whose','ARM');
+            $this->db->where('v.id',$item_id);
+            $data['item']=$this->db->get()->row_array();
+            if(!$data['item'])
+            {
+                System_helper::invalid_try('Variety Minimum Stock Setup Violation!',$item_id);
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+                $this->json_return($ajax);
+            }
+            $data['title']="Min Stock for (".$data['item']['variety_name'].')';
+            $ajax['status']=true;
+            $ajax['system_content'][]=array("id"=>"#system_content","html"=>$this->load->view($this->controller_url."/add_edit",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $ajax['system_page_url']=site_url($this->controller_url.'/index/edit/'.$item_id);
+            $this->json_return($ajax);
+        }
+        else
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->json_return($ajax);
+        }
+    }
+    private function system_save()
+    {
+        $id = $this->input->post("id");
+        $user = User_helper::get_user();
+        $time=time();
+        if(!(isset($this->permissions['action2']) && ($this->permissions['action2']==1)))
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->json_return($ajax);
+        }
+        if(!$this->check_validation())
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->message;
+            $this->json_return($ajax);
+        }
+        else
+        {
+            $variety=Query_helper::get_info($this->config->item('table_bms_setup_bud_stock_minimum'),'*',array('variety_id='.$id),1);
+            
+            $data=$this->input->post('item');
+
+            $this->db->trans_start();
+            if($variety)
+            {
+                if($variety['quantity']!=$data['quantity'])
+                {
+                    $this->db->where('variety_id',$id);
+                    $this->db->set('revision', 'revision+1', FALSE);
+                    $this->db->set('quantity',$data['quantity']);
+                    $this->db->set('date_updated',$time);
+                    $this->db->set('user_updated',$user->user_id);
+                    $this->db->update($this->config->item('table_bms_setup_bud_stock_minimum'));
+                }
+            }
+            else
+            {
+                $data['variety_id']=$id;
+                $data['revision']=1;
+                $data['user_created'] = $user->user_id;
+                $data['date_created'] = $time;
+                Query_helper::add($this->config->item('table_bms_setup_bud_stock_minimum'),$data,false);
+            }
+
+            $this->db->trans_complete();   //DB Transaction Handle END
+            if ($this->db->trans_status() === TRUE)
+            {
+                $this->message=$this->lang->line("MSG_SAVED_SUCCESS");
+                $this->system_list();
+            }
+            else
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("MSG_SAVED_FAIL");
+                $this->json_return($ajax);
+            }
+        }
+    }
+    private function check_validation()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('item[quantity]',$this->lang->line('LABEL_QUANTITY'),'required');
+        if($this->form_validation->run() == FALSE)
+        {
+            $this->message=validation_errors();
+            return false;
+        }
+        return true;
+    }
+}
---
+++ application/views/setup_bud_stock_minimum/list.php
@@ -0,0 +1,110 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$CI=& get_instance();
+$action_buttons=array();
+if(isset($CI->permissions['action2']) && ($CI->permissions['action2']==1))
+{
+    $action_buttons[]=array(
+        'type'=>'button',
+        'label'=>$CI->lang->line('ACTION_EDIT'),
+        'class'=>'button_jqx_action',
+        'data-action-link'=>site_url($CI->controller_url.'/index/edit')
+    );
+}
+if(isset($CI->permissions['action4']) && ($CI->permissions['action4']==1))
+{
+    $action_buttons[]=array(
+        'type'=>'button',
+        'label'=>$CI->lang->line("ACTION_PRINT"),
+        'class'=>'button_action_download',
+        'data-title'=>"Print",
+        'data-print'=>true
+    );
+}
+if(isset($CI->permissions['action5']) && ($CI->permissions['action5']==1))
+{
+    $action_buttons[]=array(
+        'type'=>'button',
+        'label'=>$CI->lang->line("ACTION_DOWNLOAD"),
+        'class'=>'button_action_download',
+        'data-title'=>"Download"
+    );
+}
+$action_buttons[]=array(
+    'label'=>$CI->lang->line("ACTION_REFRESH"),
+    'href'=>site_url($CI->controller_url.'/index/list')
+);
+$CI->load->view('action_buttons',array('action_buttons'=>$action_buttons));
+?>
+
+<div class="row widget">
+    <div class="widget-header">
+        <div class="title">
+            <?php echo $title; ?>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <?php
+    if(isset($CI->permissions['action6']) && ($CI->permissions['action6']==1))
+    {
+        ?>
+        <div class="col-xs-12" style="margin-bottom: 20px;">
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="crop_name"><?php echo $CI->lang->line('LABEL_CROP_NAME'); ?></label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="crop_type_name"><?php echo $CI->lang->line('LABEL_CROP_TYPE'); ?></label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="variety_name"><?php echo $CI->lang->line('LABEL_VARIETY'); ?></label>
+            <label class="checkbox-inline"><input type="checkbox" class="system_jqx_column"  checked value="quantity"><?php echo $CI->lang->line('LABEL_QUANTITY'); ?></label>
+        </div>
+        <?php
+    }
+    ?>
+    <div class="col-xs-12" id="system_jqx_container">
+
+    </div>
+</div>
+<div class="clearfix"></div>
+<script type="text/javascript">
+    $(document).ready(function ()
+    {
+        system_preset({controller:'<?php echo $CI->router->class; ?>'});
+        var url = "<?php echo base_url($CI->controller_url.'/index/get_items');?>";
+
+        // prepare the data
+        var source =
+        {
+            dataType: "json",
+            dataFields: [
+                { name: 'id', type: 'int' },
+                { name: 'crop_name', type: 'string' },
+                { name: 'crop_type_name', type: 'string' },
+                { name: 'variety_name', type: 'string' },
+                { name: 'quantity', type: 'string' }
+            ],
+            id: 'id',
+            url: url
+        };
+
+        var dataAdapter = new $.jqx.dataAdapter(source);
+        // create jqxgrid.
+        $("#system_jqx_container").jqxGrid(
+            {
+                width: '100%',
+                source: dataAdapter,
+                pageable: true,
+                filterable: true,
+                sortable: true,
+                showfilterrow: true,
+                columnsresize: true,
+                pagesize:50,
+                pagesizeoptions: ['20', '50', '100', '200','300','500'],
+                selectionmode: 'singlerow',
+                altrows: true,
+                autoheight: true,
+                columns: [
+                    { text: '<?php echo $CI->lang->line('LABEL_CROP_NAME'); ?>', dataField: 'crop_name',filtertype: 'list',width:'250'},
+                    { text: '<?php echo $CI->lang->line('LABEL_CROP_TYPE'); ?>', dataField: 'crop_type_name',width:'250'},
+                    { text: '<?php echo $CI->lang->line('LABEL_VARIETY'); ?>', dataField: 'variety_name',width:'250'},
+                    { text: '<?php echo $CI->lang->line('LABEL_QUANTITY'); ?>', dataField: 'quantity'}
+                ]
+            });
+    });
+</script>
