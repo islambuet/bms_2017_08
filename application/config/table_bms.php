21e184ecff0774759313a0c50d0fd86c0b95a787
Saiful Islam <saifultitu90@gmail.com>
2017-09-19 15:33:24 +0600

    Direct Cost Items Setup completed

--- application/config/table_bms.php
+++ application/config/table_bms.php
@@ -7,6 +7,9 @@ $config['table_bms_setup_market_size'] = 'arm_bms_2017_08.bms_setup_market_size'
 $config['table_bms_setup_currency'] = 'arm_bms_2017_08.bms_setup_currency';
 $config['table_bms_setup_currency_rate'] = 'arm_bms_2017_08.bms_setup_currency_rate';
 
+//Direct Cost Item Setup
+$config['table_bms_setup_direct_cost_items'] = 'arm_bms_2017_08.bms_setup_direct_cost_items';
+
 //Minimum Stock
 $config['table_bms_setup_bud_stock_minimum'] = 'arm_bms_2017_08.bms_setup_bud_stock_minimum';
 
